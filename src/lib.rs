f86b503b3177bbcc70551340a048af8661166588
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-06-25 15:26:27 +0200

    Create a modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+pub mod shapes;
+pub mod accelerators;
+pub mod cameras;
+pub mod samplers;
+pub mod filters;
+pub mod film;
+pub mod materials;
+pub mod textures;
+pub mod volumes;
+pub mod lights;
+pub mod renderers;
+pub mod integrators;
+
 #[cfg(test)]
 mod tests {
     #[test]
