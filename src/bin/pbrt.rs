bdec21df8f7feb2878fa30768cadbde6327d239f
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-06-27 06:03:20 +0200

    Create skeletal commandline options

--- src/bin/pbrt.rs
+++ src/bin/pbrt.rs
@@ -1,3 +1,11 @@
+#[macro_use]
+extern crate clap;
+
+use clap::App;
+
 fn main() {
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
     println!("Hello, World!");
 }
